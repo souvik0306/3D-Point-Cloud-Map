506b0faea25cbd4d4e9f23178dd857bb283a1a6a
Souvik Datta <souvikdatta123@gmail.com>
2022-10-22 17:54:28 +0530

    done

--- depth_picture.py
+++ depth_picture.py
@@ -1,7 +1,7 @@
-from cv2 import COLOR_BGR2RGB, COLOR_RGB2BGR
 import numpy as np
 import cv2 
 import time
+import matplotlib.pyplot as plt
 
 path_model = 'Depth Estimation'
 
@@ -12,12 +12,8 @@ model = cv2.dnn.readNet(r'models\model-f6b98070.onnx')
 model.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
 model.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
 
-# cap = cv2.VideoCapture(r'Photos\videoplayback.mp4')
-
-
-
     # Read in the image
-img = cv2.imread(r'Media\colorImg.jpg')
+img = cv2.imread(r'Media\colorImg2.jpg')
 img = cv2.resize(img,(640,480))
 imgHeight, imgWidth, channels = img.shape
 
@@ -32,9 +28,6 @@ img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 # MiDaS v2.1 Large ( Scale : 1 / 255, Size : 384 x 384, Mean Subtraction : ( 123.675, 116.28, 103.53 ), Channels Order : RGB )
 blob = cv2.dnn.blobFromImage(img, 1/255., (384,384), (123.675, 116.28, 103.53), True, False)
 
-# MiDaS v2.1 Small ( Scale : 1 / 255, Size : 256 x 256, Mean Subtraction : ( 123.675, 116.28, 103.53 ), Channels Order : RGB )
-#blob = cv2.dnn.blobFromImage(img, 1/255., (256,256), (123.675, 116.28, 103.53), True, False)
-
 # Set input to the model
 model.setInput(blob)
 
@@ -47,19 +40,13 @@ output = cv2.resize(output, (imgWidth, imgHeight))
 # Normalize the output
 output = cv2.normalize(output, None, 0, 1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
 
-# End time
-end = time.time()
-# calculate the FPS for current frame detection
-fps = 1 / (end-start)
-# Show FPS
-# cv2.putText(img, f"{fps:.2f} FPS", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
 img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
 
 cv2.imshow('image', img)
 cv2.imshow('Depth Map', output)
 
-# cv2.imwrite('colorImg.jpg', img)
-# cv2.imwrite('depthImg.jpg', output)
+output = output*255
+cv2.imwrite('Media\depthImg2.png', output)
+cv2.imwrite('Media\colorImg2.jpg', img)
 
 cv2.waitKey(0)
